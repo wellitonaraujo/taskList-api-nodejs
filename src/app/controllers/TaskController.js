580df98622fe766b911f1b9a062f5898709a7e69
Welliton Araujo <wellitonaraujodev@hotmail.com>
2022-03-08 16:50:40 -0300

    criando task finalizado

--- src/app/controllers/TaskController.js
+++ src/app/controllers/TaskController.js
@@ -1,8 +1,23 @@
 import Task from '../models/Task';
-
-class TaskController{
+import * as Yup from 'yup';
+class TaskController {
     async store(req, res) {
-        return res.json({ ok: true })
+        const schema = Yup.object().shape({
+            task: Yup.string().required(),
+        });
+
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({ error: "Falha ao enviar"} )
+        }
+
+        const { task } = req.body;
+
+        const tasks = await Task.create({
+            user_id: req.userId,
+            task,
+        })
+
+        return res.json(tasks)
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import TaskController from './app/controllers/TaskController';
 
 const routes = new Router();
 
@@ -15,5 +16,7 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/task', TaskController.store);
 
 export default routes;
