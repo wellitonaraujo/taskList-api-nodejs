5e97f7a98babebe04567c70945a926dcda037f85
Welliton Araujo <wellitonaraujodev@hotmail.com>
2022-03-08 10:27:33 -0300

    database configurado

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res) => {
-    return res.json( {ok: true} )
+routes.get('/teste', async (req, res) => {
+    const user = await User.create({
+        name: 'Welliton',
+        email: 'wellitonaraujodev@gmail.com',
+        password_hash: '123123'
+    })
+    return res.json( user )
 })
 
 export default routes;
